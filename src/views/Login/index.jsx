8192d15c93322393a69cb96d333cd513caabc864
AbreuLetitia <leticiaryuu@icloud.com>
2021-08-23 01:16:16 -0300

    feat: added DOM

--- src/views/Login/index.jsx
+++ src/views/Login/index.jsx
@@ -1,5 +1,18 @@
+import Link from '../../components/Link';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 const Login = () => {
-	return <div>I'm login ;-;</div>;
+	return (
+		<div>
+			<Input label="Email: " placeholder="email" type="email" />
+			<Input label="Password: " placeholder="password" type="password" />
+			<Button label="Login" />
+			<Link id="signin-link" to="/signin">
+				Sign in
+			</Link>
+		</div>
+	);
 };
 
 export default Login;
